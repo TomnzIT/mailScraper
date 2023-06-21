f6c6734b514e73c572b71803c24b0de9a87b5f29
Thomas <79933433+TomnzIT@users.noreply.github.com>
2023-06-21 20:09:26 +0200

    Update mailScraper.py

--- mailScraper.py
+++ mailScraper.py
@@ -10,24 +10,24 @@ import time
 # Create the ArgumentParser object
 parser = argparse.ArgumentParser(description="Search for email addresses associated with a domain on Google")
 
-# Add arguments
-parser.add_argument("-d", "--domain", type=str, help="Domain for email address search")
+# Add the arguments
+parser.add_argument("-d", "--domain", type=str, help="Domain to search for email addresses")
 parser.add_argument("-o", "--output", type=str, help="Output file path to save the results")
 parser.add_argument("-n", "--num-results", type=int, default=10, help="Number of results to retrieve (default: 10)")
-parser.add_argument("-p", "--proxies", type=str, help="Proxy file path")
+parser.add_argument("-p", "--proxies", type=str, help="File path containing proxies")
 
 # Parse the command-line arguments
 args = parser.parse_args()
 
 # Check if the domain is specified
 if not args.domain:
-    parser.error("Domain must be specified with the -d option")
+    parser.error("The domain must be specified with the -d option")
 
-# Perform Google search
+# Perform the Google search
 query = f'@{args.domain}'
 results = search(query, num_results=args.num_results)
 
-# Search for email addresses with the domain in the results
+# Search for email addresses in the results
 email_regex = rf'\b[A-Za-z0-9._%+-]+@{args.domain}\b'
 emails = set()
 
@@ -49,36 +49,60 @@ for result in results:
 
         # Check if proxies are available
         if proxies:
-            # Select a random proxy
-            proxy = random.choice(proxies)
+            while True:
+                # Select a random proxy
+                proxy = random.choice(proxies)
 
-            # Set the request parameters with the proxy
-            proxies = {
-                'http': proxy,
-                'https': proxy
-            }
+                # Set the request parameters with the proxy
+                proxy_dict = {
+                    'http': proxy,
+                    'https': proxy
+                }
+
+                try:
+                    # Check proxy validity using ipinfo.io
+                    response = requests.get('https://ipinfo.io/json', proxies=proxy_dict, timeout=5)
+                    response.raise_for_status()
+                    data = response.json()
+
+                    # Verify if the response contains the IP information
+                    if 'ip' in data:
+                        print(f"Valid proxy: {proxy}")
+                        break  # Proxy is valid, break out of the while loop
+                    else:
+                        print(f"Invalid proxy: {proxy}")
+                except (requests.RequestException, ValueError):
+                    # Proxy is invalid or request failed, try another proxy
+                    print(f"Invalid proxy: {proxy}")
+                    continue
+
         else:
-            proxies = None
+            proxy_dict = None
 
         # Set the User-Agent in the request headers
         headers = {'User-Agent': user_agent}
 
-        # Make the request using the proxy (if specified) and User-Agent
-        response = requests.get(result, proxies=proxies, headers=headers)
+        # Make the request using the proxy (if specified) and the User-Agent
+        response = requests.get(result, proxies=proxy_dict, headers=headers, timeout=5)
         response.raise_for_status()
 
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Search for email addresses with the domain in the HTML content
+        # Search for email addresses in the HTML content
         text = soup.get_text()
         matches = re.findall(email_regex, text)
         emails.update(matches)
 
         # Pause for 1 to 3 seconds between requests to avoid blocking
         time.sleep(random.uniform(1, 3))
+
     except requests.RequestException as e:
         # Continue execution in case of request error
-        print(f"Error retrieving content from {result}. The request will be ignored. Error: {str(e)}")
+        print(f"Error retrieving content from {result}. The request will be skipped. Error: {str(e)}")
+    except IndexError as e:
+        # Handle the case when no valid proxy is available
+        print("No valid proxy available. The request will be skipped.")
+        continue
 
 # Check if an output file is specified
 if args.output:
